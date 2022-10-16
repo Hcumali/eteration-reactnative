6d55acbac4b8668d24dca6da5a8527dea052f302
Huseyinn <hcumali16@gmail.com>
2022-10-16 17:28:55 +0300

    creating folders and indexes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
- * @format
+ * Author: Huseyin Cumali
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
